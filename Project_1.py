a9cdab1a40445dc3bc8a8fdb88307b35bc81701d
Derek Lee <dereklee@Dereks-MacBook-Air.local>
2024-06-21 18:54:26 +0800

    Second commit

--- Project_1.py
+++ Project_1.py
@@ -4,11 +4,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("templates.html")
+    return render_template("index.html")
+
+@app.route("/projects/")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/experience/")
+def experience():
+    return render_template("experience.html")
 
 
-
-
-
+    
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
